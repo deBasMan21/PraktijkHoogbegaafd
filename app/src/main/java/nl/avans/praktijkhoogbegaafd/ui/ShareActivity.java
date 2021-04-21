49c3fe37b3c6441220ea38e19a06bcb13b70095b
Bas <bbuijsen@gmail.com>
2021-04-21 21:44:21 +0200

    changes

--- app/src/main/java/nl/avans/praktijkhoogbegaafd/ui/ShareActivity.java
+++ app/src/main/java/nl/avans/praktijkhoogbegaafd/ui/ShareActivity.java
@@ -17,6 +17,7 @@ import java.util.Objects;
 
 import nl.avans.praktijkhoogbegaafd.BuildConfig;
 import nl.avans.praktijkhoogbegaafd.R;
+import nl.avans.praktijkhoogbegaafd.logic.CachedFileProvider;
 import nl.avans.praktijkhoogbegaafd.logic.ScreenshotLogic;
 
 public class ShareActivity extends AppCompatActivity {
@@ -38,33 +39,56 @@ public class ShareActivity extends AppCompatActivity {
         Uri uri = FileProvider.getUriForFile(this,
                 getApplicationContext().getPackageName() + ".provider", file);
 
-//        Uri uri2 = Uri.parse(file.getPath());
+        Uri uri2 = Uri.parse(file.getPath());
+
+        File file1 = new File(uri2.getPath());
+        if(file1.exists() && file1.canRead()){
+            System.out.println("GELUKT");
+        }
 
         Intent i = new Intent(Intent.ACTION_SENDTO);
         i.setData(Uri.parse("mailto:"));
-        i.putExtra(Intent.EXTRA_STREAM, uri);
+//        i.setType("message/rfc822");
+//        i.putExtra(Intent.EXTRA_STREAM, uri);
+        i.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://" + CachedFileProvider.AUTHORITY + "/" + file.getName()));
         startActivity(Intent.createChooser(i, "Send email..."));
 
     }
 
 
     public File storeScreenshot(Bitmap bitmap, String filename) {
-        ContextWrapper cw = new ContextWrapper(this);
-        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
-        File file = new File(directory, filename + ".jpg");
-        file.setReadable(true);
-        if (file.exists()) {
-            file.delete();
-        }
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-            fos.flush();
-            fos.close();
-        } catch (java.io.IOException e) {
+        FileOutputStream fout = null;
+        File tempFile = null;
+        try{
+            File tempDir = this.getCacheDir();
+            tempFile = File.createTempFile("your_file", ".txt", tempDir);
+            fout = new FileOutputStream(tempFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fout);
+            fout.flush();
+            fout.close();
+
+        }catch (Exception e){
             e.printStackTrace();
         }
-        return file;
+//
+//
+//
+//        ContextWrapper cw = new ContextWrapper(this);
+//        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+//        File file = new File(directory, filename + ".jpg");
+//        file.setReadable(true);
+//        if (file.exists()) {
+//            file.delete();
+//        }
+//        FileOutputStream fos = null;
+//        try {
+//            fos = new FileOutputStream(file);
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+//            fos.flush();
+//            fos.close();
+//        } catch (java.io.IOException e) {
+//            e.printStackTrace();
+//        }
+        return tempFile;
     }
 }
